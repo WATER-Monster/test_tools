8bdb69f169d3d9dc06fdb7f24cfc1e90e37b8d21
DESKTOP-A4FEECK\水怪 <826205363@>
2021-01-04 13:12:37 +0800

    修改map匹配正则

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ API_NAME_PATTERN = r"###(.*)"
 API_URL_PATTERN = r"> \*\*url:[\s*]?(.*)[\s*]?\*\*"
 API_METHODS_PATTERN = r"> \*\*[\s*]?methods:[\s*]?(.*)[\s*]?\*\*"
 API_CONTENT_TYPE = r"> \*\*[\s*]?Content-Type:[\s*]?(.*)[\s*]?\*\*"
-API_PARAM = r"\[//\]:[\s*]?param(.*)\[//\]:[\s*]?param"
-API_RES_TRUE = r"\[//\]:[\s*]?True-response(.*)\[//\]:[\s*]?True-response"
-API_RES_FALSE = r"\[//\]:[\s*]?False-response(.*)\[//\]:[\s*]?False-response"
+API_PARAM = r"\[//\]:[\s*]?param\n```\w+(.*)```\n\[//\]:[\s*]?param"
+API_RES_TRUE = r"\[//\]:[\s*]?True-response\n```\w+(.*)```\n\[//\]:[\s*]?True-response"
+API_RES_FALSE = r"\[//\]:[\s*]?False-response\n```\w+(.*)```\n\[//\]:[\s*]?False-response"
 
 PARAM_N = 10 # 容错测试中循环次数
 
